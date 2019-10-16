a48051af738330153737bde8e3125f4315268cb9
Sashikanth Arepalli -X (saarepal - TECH MAHINDRA LIM at Cisco) <saarepal@partnet.cisco.com>
2019-10-16 20:04:55 +0530

    My first github commit file

---
+++ Git_hub_Repository_example1.py
@@ -0,0 +1,23 @@
+
+# coding: utf-8
+
+# ## This notebook will be mainly used for the capstone project
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[1]:
+
+
+print("Going good")
+
